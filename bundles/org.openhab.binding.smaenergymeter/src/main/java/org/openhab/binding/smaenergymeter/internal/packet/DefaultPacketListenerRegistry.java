6cd207561caa99cde615926c81104450477aa0f8
Łukasz Dywicki <luke@code-house.org>
2021-12-05 17:48:50 +0100

    [smaenergymeter] Ignore frames which are using protocol identifier different than e-meter.
    Fix handling of broadcast frames for SMA meter #11497.
    
    Added support for multiple meters in single multicast group #3429.
    
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

---
+++ bundles/org.openhab.binding.smaenergymeter/src/main/java/org/openhab/binding/smaenergymeter/internal/packet/DefaultPacketListenerRegistry.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.smaenergymeter.internal.packet;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of packet listener registry which manage multicast sockets.
+ *
+ * @author Łukasz Dywicki - Initial contribution
+ */
+@Component
+public class DefaultPacketListenerRegistry implements PacketListenerRegistry {
+
+    private final Logger logger = LoggerFactory.getLogger(DefaultPacketListenerRegistry.class);
+    private final Map<String, PacketListener> listeners = new ConcurrentHashMap<>();
+
+    @Override
+    public PacketListener getListener(String group, int port) throws IOException {
+        String identifier = group + ":" + port;
+        PacketListener listener = listeners.get(identifier);
+        if (listener == null) {
+            listener = new PacketListener(this, group, port);
+            listeners.put(identifier, listener);
+        }
+        return listener;
+    }
+
+    @Deactivate
+    protected void shutdown() throws IOException {
+        for (Entry<String, PacketListener> entry : listeners.entrySet()) {
+            try {
+                entry.getValue().close();
+            } catch (IOException e) {
+                logger.warn("Multicast socket {} failed to terminate", entry.getKey(), e);
+            }
+        }
+    }
+
+    public void close(String group, int port) {
+        String listenerId = group + ":" + port;
+        PacketListener listener = listeners.remove(listenerId);
+        if (listener != null) {
+            try {
+                listener.close();
+            } catch (IOException e) {
+                logger.warn("Multicast socket {} failed to terminate", listenerId, e);
+            }
+        }
+    }
+}
